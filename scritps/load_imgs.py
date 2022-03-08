5b225c5c4bee2b080e35050531ffaba5b4551134
LeDSantos <d.santos.leti@gmail.com>
2022-03-08 16:31:46 -0300

    Teste

--- scritps/load_imgs.py
+++ scritps/load_imgs.py
@@ -1,2 +1,9 @@
 import cv2
 import sys
+
+
+img = cv2.imread('/home/leticia/Documentos/ComputacionalVision/ComputerVision/imgs/composite_01.png')
+
+cv2.namedWindow('Janela')
+cv2.imshow('Janela', img)
+cv2.waitKey()
