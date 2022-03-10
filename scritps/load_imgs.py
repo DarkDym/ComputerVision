d04171929e2185c364ab333ff8ee1a6b9b540ed4
LeDSantos <d.santos.leti@gmail.com>
2022-03-10 16:49:12 -0300

    Questão 1 feita

--- scritps/load_imgs.py
+++ scritps/load_imgs.py
@@ -6,13 +6,20 @@ import numpy as np
 
 maior_dif=0
 i_img=-1
-img = []
+imagens = []
 for i in range(0,4):
-    img.append(cv2.imread('/home/leticia/Documentos/ComputacionalVision/ComputerVision/imgs/composite_0'+str(i+1)+'.png'))
+    imagens.append(cv2.imread('/home/leticia/Documentos/ComputacionalVision/ComputerVision/imgs/composite_0'+str(i+1)+'.png'))
     # cv2.namedWindow('Janela'+str(i))
     # cv2.imshow('Janela'+str(i), img[i])
 
-    B, G, R = cv2.split(img[i])
+    height, width = imagens[i].shape[:2]
+    norm=np.zeros((height,width))
+    final=cv2.normalize(imagens[i], norm, 0, 255, cv2.NORM_MINMAX)
+
+    # cv2.namedWindow('JanelaNORMALIZADO'+str(i))
+    # cv2.imshow('JanelaNORMALIZADO'+str(i), final)
+
+    B, G, R = cv2.split(final)
     diferenca=np.average(R-B)
     # print(diferenca)
     if diferenca > maior_dif:
@@ -21,3 +28,12 @@ for i in range(0,4):
 # cv2.waitKey()
 
 print('Escolhido '+str(i_img+1)+' com '+str(maior_dif))
+
+B, G, R = cv2.split(imagens[i_img])
+height, width = imagens[i_img].shape[:2]
+gray_image = np.full((height, width), R-B, dtype=np.uint8)
+
+cv2.namedWindow('ESCOLHIDA')
+cv2.imshow('ESCOLHIDA', gray_image)
+print("QUESTÃO 1 FEITA")
+cv2.waitKey()
