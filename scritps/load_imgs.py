89d2cb16aa6fda7da03dd26b1d38f82563215239
LeDSantos <d.santos.leti@gmail.com>
2022-03-10 16:23:57 -0300

    Acha img com maior diferença entre R e B

--- scritps/load_imgs.py
+++ scritps/load_imgs.py
@@ -1,9 +1,23 @@
 import cv2
-import sys
+import os, sys
 
+import math
+import numpy as np
 
-img = cv2.imread('/home/leticia/Documentos/ComputacionalVision/ComputerVision/imgs/composite_01.png')
+maior_dif=0
+i_img=-1
+img = []
+for i in range(0,4):
+    img.append(cv2.imread('/home/leticia/Documentos/ComputacionalVision/ComputerVision/imgs/composite_0'+str(i+1)+'.png'))
+    # cv2.namedWindow('Janela'+str(i))
+    # cv2.imshow('Janela'+str(i), img[i])
 
-cv2.namedWindow('Janela')
-cv2.imshow('Janela', img)
-cv2.waitKey()
+    B, G, R = cv2.split(img[i])
+    diferenca=np.average(R-B)
+    # print(diferenca)
+    if diferenca > maior_dif:
+        maior_dif = diferenca
+        i_img=i
+# cv2.waitKey()
+
+print('Escolhido '+str(i_img+1)+' com '+str(maior_dif))
