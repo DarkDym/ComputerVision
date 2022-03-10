0e2f183d6a0b9b10ccb719689253dd0fda239348
LeDSantos <d.santos.leti@gmail.com>
2022-03-10 16:53:56 -0300

    Questão 1 corrigida, gray para todas as imagens

--- scritps/load_imgs.py
+++ scritps/load_imgs.py
@@ -7,6 +7,7 @@ import numpy as np
 maior_dif=0
 i_img=-1
 imagens = []
+gray_images = []
 for i in range(0,4):
     imagens.append(cv2.imread('/home/leticia/Documentos/ComputacionalVision/ComputerVision/imgs/composite_0'+str(i+1)+'.png'))
     # cv2.namedWindow('Janela'+str(i))
@@ -20,20 +21,11 @@ for i in range(0,4):
     # cv2.imshow('JanelaNORMALIZADO'+str(i), final)
 
     B, G, R = cv2.split(final)
-    diferenca=np.average(R-B)
-    # print(diferenca)
-    if diferenca > maior_dif:
-        maior_dif = diferenca
-        i_img=i
-# cv2.waitKey()
+    gray_images.append(np.full((height, width), R-B, dtype=np.uint8))
+    cv2.namedWindow('JanelaGRAY'+str(i))
+    cv2.imshow('JanelaGRAY'+str(i), gray_images[i])
 
-print('Escolhido '+str(i_img+1)+' com '+str(maior_dif))
+cv2.waitKey()
 
-B, G, R = cv2.split(imagens[i_img])
-height, width = imagens[i_img].shape[:2]
-gray_image = np.full((height, width), R-B, dtype=np.uint8)
-
-cv2.namedWindow('ESCOLHIDA')
-cv2.imshow('ESCOLHIDA', gray_image)
 print("QUESTÃO 1 FEITA")
 cv2.waitKey()
