97e0b87d06a1ce09af08c34db16e9bf20a305c7a
LeDSantos <d.santos.leti@gmail.com>
2022-03-14 19:48:44 -0300

    Questão 2

--- scritps/load_imgs.py
+++ scritps/load_imgs.py
@@ -48,10 +48,36 @@ for i in range(0,4):
     # plt.hist(img_hist.ravel(), max_value ,[0, max_value])
     # plt.show()
 
+    #histograma=plt.hist(images[i*3].ravel(),256)
+    #plt.axvline(np.mean(img_hist), color='k', linestyle='dashed', linewidth=5)
+
+
+    initial_th=np.mean(img_hist)
+    ret_first,th_first = cv2.threshold(img_hist,initial_th,255,cv2.THRESH_BINARY)
+    second_th=np.mean(th_first)
+    ret_second,th_second = cv2.threshold(img_hist,second_th,255,cv2.THRESH_BINARY)
+    cv2.imshow('Janela'+str(i), th_second)
+    cv2.waitKey()
+
+
+    # img_low=np.zeros((height,width))
+    # img_high=np.zeros((height,width))
+
+    # for pixel in img_hist:
+    #     if pixel < initial_th:
+    #         img_low=pixel
+    #     else:
+    #         img_high=pixel
+
+    # cv2.imshow('HIGH', img_high)
+
+
     # FONTE: https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
     
+    # teste=127
+    teste=55
     # global thresholding
-    ret1,th1 = cv2.threshold(img_hist,127,255,cv2.THRESH_BINARY)
+    ret1,th1 = cv2.threshold(img_hist,teste,255,cv2.THRESH_BINARY)
     # Otsu's thresholding
     ret2,th2 = cv2.threshold(img_hist,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # Otsu's thresholding after Gaussian filtering
@@ -61,7 +87,7 @@ for i in range(0,4):
     images = [img_hist, 0, th1,
             img_hist, 0, th2,
             blur, 0, th3]
-    titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+    titles = ['Original Noisy Image','Histogram','Global Thresholding (v=)'+str(teste),
             'Original Noisy Image','Histogram',"Otsu's Thresholding",
             'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
     for i in range(3):
@@ -69,8 +95,24 @@ for i in range(0,4):
         plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
         plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
         plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+
+
+
+        #         Select initial threshold value, typically the mean 8-bit value of the original image.
+        # Divide the original image into two portions;
+        #     Pixel values that are less than or equal to the threshold; background
+        #     Pixel values greater than the threshold; foreground
+        # Find the average mean values of the two new images
+        # Calculate the new threshold by averaging the two means.
+        # If the difference between the previous threshold value and the new threshold value are below a specified limit, you are finished. Otherwise apply the new threshold to the original image keep trying.
+
+
+
         ax = plt.gca()
         # ax.set_xlim([xmin, xmax])
+
+
+
         ax.set_ylim([0, 100])
         plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
         plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
---
+++ scritps/questao2.py
@@ -0,0 +1,115 @@
+import cv2
+import os, sys
+import glob
+
+import math
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+PATH_TO_IMGS = "./imgs/"
+PATH_TO_IMGS_Q1 = "./questao1/"
+PATH_TO_IMGS_Q2 = "./questao2/"
+
+class Questao1:
+    def __init__(self):
+
+        files = self.openDir()
+
+        for img_file in files:
+            # Carrega imagem
+            img = cv2.imread(img_file)
+            print("Usando imagem: "+img_file)
+            img_file_out=img_file.split("/")[-1]
+            # cv2.imshow('Imagem '+img_file_out, img)
+            # cv2.waitKey()
+            # cv2.destroyAllWindows()
+
+            # Normaliza para ajudar no processamento posterior
+            height, width = img.shape[:2]
+            norm=np.zeros((height,width))
+            final=cv2.normalize(img, norm, 0, 255, cv2.NORM_MINMAX)
+            # cv2.imshow('Imagem NORMALIZADA '+img_file_out, final)
+            # cv2.waitKey()
+            # cv2.destroyAllWindows()
+
+            # Faz a diferença entre os canais e salva
+            B, G, R = cv2.split(final)
+            gray_image=np.full((height, width), R-B, dtype=np.uint8)
+            cv2.imwrite(PATH_TO_IMGS_Q1+ img_file_out, gray_image)
+            # cv2.imshow('Imagem GRAY '+img_file_out, gray_image)
+            # cv2.waitKey()
+            # cv2.destroyAllWindows()
+        
+        print("questao1 executada")
+
+
+    def openDir(self):
+        files = glob.glob(PATH_TO_IMGS+"*.png")
+        # print(files)
+        return files
+
+
+class Questao2:
+    def __init__(self):
+        files = self.openDir()
+
+        for img_file in files:
+            # Carrega imagem
+            img = cv2.imread(img_file)
+            img_file_out=img_file.split("/")[-1]
+            height, width = img.shape[:2]
+            norm=np.zeros((height,width))
+            final=cv2.normalize(img, norm, 0, 255, cv2.NORM_MINMAX)
+            B, G, R = cv2.split(final)
+            gray_image=np.full((height, width), R-B, dtype=np.uint8)
+
+            # Estratégia proposta por nós
+            initial_th=np.mean(gray_image)
+            ret_first,img_first = cv2.threshold(gray_image,initial_th,255,cv2.THRESH_BINARY)
+            # cv2.imshow('Janela', img_first)
+            # cv2.waitKey()
+            second_th=np.mean(img_first)
+            ret_second,img_our_method = cv2.threshold(gray_image,second_th,255,cv2.THRESH_BINARY)
+
+            # plot all the images and their histograms
+            images = [gray_image, 0, img_our_method]
+            titles = ['Original Gray Image','Histogram','Global Thresholding '+str(int(second_th))]
+
+            # Plot da imagem original
+            plt.subplot(1,3,1),plt.imshow(gray_image,'gray')
+            plt.title(titles[0]), plt.xticks([]), plt.yticks([])
+
+            # Histograma com eixo y limitado para facilitar a visualização
+            plt.subplot(1,3,2),plt.hist(gray_image.ravel(),256)
+            plt.title(titles[1]), plt.xticks([]), plt.yticks([])
+            ax = plt.gca()
+            ax.set_ylim([0, 100])
+            # Primeiro limiar em preto
+            plt.axvline(int(initial_th), color='k', linestyle='dashed', linewidth=2)
+            # Segundo e limiar final em vermelho
+            plt.axvline(int(second_th), color='r', linestyle='dashed', linewidth=2)
+
+            # Plot da imagem resultante do nosso método
+            plt.subplot(1,3,3),plt.imshow(img_our_method,'gray')
+            plt.title(titles[2]), plt.xticks([]), plt.yticks([])
+
+            print(img_file_out.split(".")[0])
+            plt.savefig(PATH_TO_IMGS_Q2+img_file_out.split(".")[0]+"a.png")
+            plt.close()
+            #plt.show()
+
+        print("questao2 executada")
+
+    def openDir(self):
+        files = glob.glob(PATH_TO_IMGS+"*.png")
+        # print(files)
+        return files      
+
+if __name__ == "__main__":
+    
+    # Questao1().openDir()
+
+    Questao2().openDir()
+
+
